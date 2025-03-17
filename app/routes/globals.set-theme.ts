979b2ccabdd186d91d608dbba7fac58718cae621
Luiz Eduardo <eduardo_jr95@hotmail.com>
2025-03-16 23:20:07 -0300

    Refactor theme management and clean up global styles
    - Updated the theme management route by removing unnecessary loader function.
    - Cleaned up globals.css by removing deprecated custom theme variables to streamline styling.

--- app/routes/globals.set-theme.ts
+++ app/routes/globals.set-theme.ts
@@ -1,10 +1,4 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
 import { themeSessionResolver } from "@services/theme/sessions.server";
 import { createThemeAction } from "remix-themes";
 
-// export const action = createThemeAction(themeSessionResolver);
 export const action = createThemeAction(themeSessionResolver);
-export async function loader({ request }: LoaderFunctionArgs) {
-  const { getTheme } = await themeSessionResolver(request);
-  return { theme: getTheme() };
-}
